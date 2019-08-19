d7a6ff30c73d90c8063b492214d2b4d0ae4a986b
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-08-19 16:02:22 -0700

    a test passed at all

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
 // Test away
+
+import React from "react";
+import ReactDOM from 'react-dom';
+import { render, fireEvent, act } from "@testing-library/react";
+import Dashboard from "./Dashboard";     
+
+it('dashboard renders controls and display', () => {
+    const { container } = render(<Dashboard />);
+    const controls = container.querySelector('.controls.panel');
+    expect(controls).toBeTruthy();
+});
